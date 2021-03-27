eab1b37b723a64b4a8d854a04878ba6ffe7a2cf2
KorogodDV <70817425+KorogodDV@users.noreply.github.com>
2021-03-27 22:36:08 +0300

    added nonworking storage and fixed codestyle

---
+++ Storage.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <list>
+#include <string>
+#include "GameObject.h"
+
+class Storage {
+private:
+
+	std::list<GameObject> GameObjects;
+
+public:
+
+	void addObject(std::string name)
+	{
+
+	}
+};
