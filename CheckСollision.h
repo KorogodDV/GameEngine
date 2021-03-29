5e6181a4de1696164f0ed68356b89b8478d6893e
Дмитрий <@LAPTOP-2DB0HTOT>
2021-03-29 17:57:34 +0300

    CheckCollision changed

--- CheckСollision.h
+++ CheckСollision.h
@@ -1,22 +1,32 @@
 #pragma once
 
-float scalar(sf::Vector2f v_1, sf::Vector2f v_2 )
+float scalar_prod(sf::Vector2f v_1, sf::Vector2f v_2 )
 {
 	return v_1.x * v_2.x + v_1.y * v_2.y;
 }
 
-sf::Vector2f cord_Pr1(sf::ConvexShape Hitbox, sf::Vector2f line,  sf::Vector2f point_0)
+sf::Vector2f projection(sf::Vector2f v_1, sf::Vector2f v_2)
 {
-	float x_min = (line * (scalar(line, (Hitbox.getPoint(0) - point_0))) / (scalar(line, line))).x;
-	float x_max = (line * (scalar(line, (Hitbox.getPoint(0) - point_0))) / (scalar(line, line))).x;
+	return v_1 * scalar_prod(v_1, v_2) / scalar_prod(v_1, v_1);
+}
+
+sf::Vector2f ort(sf::Vector2f v)
+{
+	return sf::Vector2f(-v.y, v.x);
+}
+
+sf::Vector2f cord_Pr(sf::ConvexShape Hitbox, sf::Vector2f line,  sf::Vector2f point_0)
+{
+	float x_min = (projection(line, Hitbox.getPoint(0) - point_0)).x;
+	float x_max = (projection(line, Hitbox.getPoint(0) - point_0)).x;
 
 	for (int j = 1; j < Hitbox.getPointCount(); j++)
 	{
-		if ((line * (scalar(line, (Hitbox.getPoint(j) - point_0))) / (scalar(line, line))).x < x_min)
-			x_min = (line * (scalar(line, (Hitbox.getPoint(j) - point_0))) / (scalar(line, line))).x;
+		if ((projection(line, Hitbox.getPoint(j) - point_0)).x < x_min)
+			x_min = (projection(line, Hitbox.getPoint(j) - point_0)).x;
 
-		if ((line * (scalar(line, (Hitbox.getPoint(j) - point_0))) / (scalar(line, line))).x > x_max)
-			x_max = (line * (scalar(line, (Hitbox.getPoint(j) - point_0))) / (scalar(line, line))).x;
+		if ((projection(line, Hitbox.getPoint(j) - point_0)).x > x_max)
+			x_max = (projection(line, Hitbox.getPoint(j) - point_0)).x;
 	}
 	return sf::Vector2f(x_min, x_max);
 }
@@ -25,11 +35,11 @@ bool checkCollision(sf::ConvexShape Hitbox1, sf::ConvexShape Hitbox2)
 {
 	for (int i = 0; i < Hitbox1.getPointCount(); i++)
 	{
-		sf::Vector2f cord_Pr_Hitbox1 = cord_Pr1( Hitbox1, sf::Vector2f((Hitbox1.getPoint((i + 1) % Hitbox1.getPointCount()) - Hitbox1.getPoint(i)).y, -(Hitbox1.getPoint((i + 1) % Hitbox1.getPointCount()) - Hitbox1.getPoint(i)).x), Hitbox1.getPoint(i));
+		sf::Vector2f cord_Pr_Hitbox1 = cord_Pr(Hitbox1, ort(Hitbox1.getPoint((i + 1) % Hitbox1.getPointCount()) - Hitbox1.getPoint(i)), Hitbox1.getPoint(i));
 		float x_min_Hitbox1 = cord_Pr_Hitbox1.x;
 		float x_max_Hitbox1 = cord_Pr_Hitbox1.y;
 
-		sf::Vector2f cord_Pr_Hitbox2 = cord_Pr1(Hitbox2, sf::Vector2f((Hitbox1.getPoint((i + 1) % Hitbox1.getPointCount()) - Hitbox1.getPoint(i)).y, -(Hitbox1.getPoint((i + 1) % Hitbox1.getPointCount()) - Hitbox1.getPoint(i)).x), Hitbox1.getPoint(i));
+		sf::Vector2f cord_Pr_Hitbox2 = cord_Pr(Hitbox2, ort(Hitbox1.getPoint((i + 1) % Hitbox1.getPointCount()) - Hitbox1.getPoint(i)), Hitbox1.getPoint(i));
 		float x_min_Hitbox2 = cord_Pr_Hitbox2.x;
 		float x_max_Hitbox2 = cord_Pr_Hitbox2.y;
 
@@ -37,5 +47,41 @@ bool checkCollision(sf::ConvexShape Hitbox1, sf::ConvexShape Hitbox2)
 			return false;
 	}
 	return true;
+}
+
+void mymove(sf::ConvexShape* Hitbox, sf::Vector2f speed, float t)
+{
+	for (int i = 0; i < Hitbox->getPointCount(); i++)
+	{
+		Hitbox->setPoint(i, Hitbox->getPoint(i) + speed * t);
+	}
+}
+
+void move(sf::ConvexShape* Hitbox, sf::Vector2f speed,  float t)
+{
+	Hitbox->move(speed * t);
+}
+
+void collide(sf::ConvexShape* Hitbox1, sf::ConvexShape* Hitbox2, sf::Vector2f speed1, sf::Vector2f speed2, float t)
+{
+	sf::Vector2f Pr_speed1 = projection(speed1 + speed2, speed1);
+	sf::Vector2f Pr_speed2 = projection(speed1 + speed2, speed2);
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (checkCollision(*Hitbox1, *Hitbox2))
+		{
+			mymove(Hitbox1, -Pr_speed1, t);
+			mymove(Hitbox2, -Pr_speed2, t);
+		}
+		else
+		{
+			mymove(Hitbox1, Pr_speed1, t);
+			mymove(Hitbox2, Pr_speed2, t);
+		}
+		
+		Pr_speed1 = Pr_speed1 / 2.f;
+		Pr_speed2 = Pr_speed2 / 2.f;
+	}
 }
 
--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 const int window_length = 1280;
 const int window_width = 720;
 
+/*
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(window_length, window_width), "test");
@@ -55,5 +56,79 @@ int main()
             }
         }
     }
+    return 0;
+}*/
+
+int main()
+{
+    float t = 0.1;
+    sf::RenderWindow window(sf::VideoMode(window_length, window_width), "test");
+
+    sf::ConvexShape Hitbox1(3);
+    sf::ConvexShape Hitbox2(3);
+    
+    Hitbox1.setFillColor(sf::Color::Red);
+    Hitbox2.setFillColor(sf::Color::Green);
+
+    Hitbox1.setPoint(0, sf::Vector2f(200, 200));
+    Hitbox1.setPoint(1, sf::Vector2f(250, 250));
+    Hitbox1.setPoint(2, sf::Vector2f(200, 250));
+
+    Hitbox2.setPoint(0, sf::Vector2f(500, 550));
+    Hitbox2.setPoint(1, sf::Vector2f(450, 500));
+    Hitbox2.setPoint(2, sf::Vector2f(500, 450));
+    
+    //std::cout << checkCollision(Hitbox1, Hitbox2) << std::endl;
+
+    sf::Vector2f speed1(1,1);
+    sf::Vector2f speed2(-1, -1);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        window.clear();
+        window.draw(Hitbox1);
+        window.draw(Hitbox2);
+        window.display();
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+                break;
+            }
+        }
+
+        if (checkCollision(Hitbox1, Hitbox2))
+        {
+            std::cout << 1 << std::endl;
+            collide(&Hitbox1, &Hitbox2, speed1, speed2, t);
+            sf::Vector2f speed = speed1;
+            speed1 = sf::Vector2f(0, 0);
+            speed2 = sf::Vector2f(0, 0);
+        }
+
+        if ((Hitbox1.getPoint(0).x < 0) or (Hitbox1.getPoint(0).x > 1150))
+        {
+            speed1.x = -speed1.x;
+        }
+        if ((Hitbox1.getPoint(0).y < 0) or (Hitbox1.getPoint(0).y > 670))
+        {
+            speed1.y = -speed1.y;
+        }
+        if ((Hitbox2.getPoint(0).x < 0) or (Hitbox2.getPoint(0).x > 1150))
+        {
+            speed2.x = -speed2.x;
+        }
+        if ((Hitbox2.getPoint(0).y < 0) or (Hitbox2.getPoint(0).y > 670))
+        {
+            speed2.y = -speed2.y;
+        }
+
+        mymove(&Hitbox1, speed1, t);
+        mymove(&Hitbox2, speed2, t);
+    }
+    
     return 0;
 }
