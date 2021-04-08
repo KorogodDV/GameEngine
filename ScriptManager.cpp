809c48ac54e4b6d61834cb2a7f36bfb8d710261a
KorogodDV <70817425+KorogodDV@users.noreply.github.com>
2021-04-08 11:19:14 +0300

    Added private field app, constructor and GetApp(), ObjectOnCollide()

--- ScriptManager.cpp
+++ ScriptManager.cpp
@@ -3,30 +3,51 @@
 #include "Components.h"
 #include "GameObject.h"
 #include "ScriptManager.h"
+#include "Application.h"
+
+ScriptManagers::ScriptManagers(Applications* app)
+{
+    scripts = *new std::list<Script*>;
+    this->app = app;
+}
+
+Applications* ScriptManagers::GetApplication()
+{
+    return app;
+}
 
 void ScriptManagers::AddScript(Script* newscript)
-    {
-        scripts.push_back(newscript);
-    }
+{
+    scripts.push_back(newscript);
+}
 
 void ScriptManagers::RemoveScript(Script* script)
-    {
-        scripts.remove(script);
-    }
+{
+    scripts.remove(script);
+}
 
 void ScriptManagers::RemoveScript(GameObject* obj)
-    {
-        scripts.remove_if([obj](Script* script) { return  script->gameObject == obj; });
-    }
+{
+    scripts.remove_if([obj](Script* script) { return  script->gameObject == obj; });
+}
 
- void ScriptManagers::update()
+void ScriptManagers::ObjectOnCollide(GameObject* obj)
+{
+    for (Script* script : scripts)
     {
-        for (Script* currscript : scripts)
-            currscript->execute();
+        if (script->gameObject->name == obj->name)
+            script->onCollide();
     }
+}
+
+void ScriptManagers::update()
+{
+    for (Script* currscript : scripts)
+        currscript->execute();
+}
 
 int ScriptManagers::size()
-    {
-        return scripts.size();
-    }
+{
+    return scripts.size();
+}
 
--- ScriptManager.h
+++ ScriptManager.h
@@ -3,19 +3,27 @@
 #include "Components.h"
 
 class GameObject;
+class Applications;
 
 class ScriptManagers {
 private:
 
     std::list<Script*> scripts;
+    Applications* app;
 
 public:
+
+    ScriptManagers(Applications* app);
+
+    Applications* GetApplication();
 
     void AddScript(Script* newscript);
 
     void RemoveScript(Script* script);
 
     void RemoveScript(GameObject* obj);
+
+    void ObjectOnCollide(GameObject* obj);
 
     void update();
 
