e0a09e1c41c4e73b66d4e7d8f44a7495b80c1ff7
KorogodDV <70817425+KorogodDV@users.noreply.github.com>
2021-05-16 22:11:07 +0300

    minor gameplay fixes

--- Components.cpp
+++ Components.cpp
@@ -10,8 +10,6 @@
 void Move::execute()
 {
     gameObject->GetComponent<Physics>()->speed += gameObject->GetComponent<Physics>()->acceleration * gameObject->GetApplication()->GetLastFrameDurationAsSeconds();
-   if (len(gameObject->GetComponent<Physics>()->speed) > 40000)
-        gameObject->GetComponent<Physics>()->speed = normalize(gameObject->GetComponent<Physics>()->speed) * 200.f;
     gameObject->GetComponent<Physics>()->pos += gameObject->GetComponent<Physics>()->speed * gameObject->GetApplication()->GetLastFrameDurationAsSeconds();
     (gameObject->GetComponent<Renderer>()->sprite).move(gameObject->GetApplication()->GetLastFrameDurationAsSeconds() * gameObject->GetComponent<Physics>()->speed);
     for (int i = 0; i < gameObject->GetComponent<Collider>()->hitboxes.begin()->getPointCount(); i++)
--- main.cpp
+++ main.cpp
@@ -172,7 +172,7 @@ public:
                     std::to_string(int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds() * 1000)) +
                     " bullet.png " + std::to_string(this->gameObject->GetComponent<Physics>()->pos.x - 50) + " " +
                     std::to_string(this->gameObject->GetComponent<Physics>()->pos.y) + " " +
-                    std::to_string(-300) + " 0 " + std::to_string(length) + " " + std::to_string(width) + " 0");
+                    std::to_string(-300 * (int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 9 + 1)) + " 0 " + std::to_string(length) + " " + std::to_string(width) + " 0");
                 this->gameObject->GetApplication()->GetStorage()->GetObject(std::string("bullet2_") +
                     std::to_string(int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds() * 1000)))->AddComponent<Bullet>();
                 this->gameObject->GetApplication()->GetStorage()->GetObject(std::string("bullet2_") +
@@ -191,19 +191,20 @@ public:
         {
             float width;
             float length;
+            int amound_of_spawning_enemies = 6 + int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 15;
             std::random_device rd;
             switch(rd() % 5)
             {
             case 0: case 1:
                     width = 50;
                     length = 100;
-                    for (int i = 0; i < 6; i++)
+                    for (int i = 0; i < amound_of_spawning_enemies; i++)
                     {
                         this->gameObject->GetApplication()->GetStorage()->CreateUnit("fast_enemy " + std::string("fast_enemy") + 
                             std::to_string(int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 3) + std::to_string(i + 1) + 
                             " fast_enemy.png " + std::to_string(this->gameObject->GetApplication()->GetWindow()->getSize().x + (length + 1)* i) + " "  + 
                             std::to_string(rand() % int(this->gameObject->GetApplication()->GetWindow()->getSize().y - 2 * width) + width) + " " + 
-                            std::to_string(-150 * (int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 9 + 1)) +
+                            std::to_string(-150 * (int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 18 + 1)) +
                             " 0 " + std::to_string(length) + " " + std::to_string(width) + " 1 0 1");
                         this->gameObject->GetApplication()->GetStorage()->GetObject(std::string("fast_enemy") +
                             std::to_string(int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 3) + std::to_string(i + 1))->AddComponent<ReturnToField>();
@@ -214,13 +215,13 @@ public:
             case 2: case 3:
                     width = 20;
                     length = 40;
-                    for (int i = 0; i < 6; i++)
+                    for (int i = 0; i < amound_of_spawning_enemies; i++)
                     {
                         this->gameObject->GetApplication()->GetStorage()->CreateUnit("slow_enemy " + std::string("slow_enemy") +
                             std::to_string(int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 3) + std::to_string(i + 1) +
                             " slow_enemy.png " + std::to_string(this->gameObject->GetApplication()->GetWindow()->getSize().x + (length + 1) * i) + " " +
                             std::to_string(rand() % int(this->gameObject->GetApplication()->GetWindow()->getSize().y - 2 * width) + width) + " " +
-                            std::to_string(-100 * (int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 9 + 1)) +
+                            std::to_string(-100 * (int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 18 + 1)) +
                             " 0 " + std::to_string(length) + " " + std::to_string(width) + " 1 0 1");
                         this->gameObject->GetApplication()->GetStorage()->GetObject(std::string("slow_enemy") +
                             std::to_string(int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 3) + std::to_string(i + 1))->AddComponent<ReturnToField>();
@@ -231,13 +232,13 @@ public:
                 case 4:
                     width = 50;
                     length = 50;
-                    for (int i = 0; i < 6; i++)
+                    for (int i = 0; i < amound_of_spawning_enemies; i++)
                     {
                         this->gameObject->GetApplication()->GetStorage()->CreateUnit("shooting_enemy " + std::string("shooting_enemy") +
                             std::to_string(int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 3) + std::to_string(i + 1) +
                             " shooting_enemy.png " + std::to_string(this->gameObject->GetApplication()->GetWindow()->getSize().x + (length + 1) * i) + " " +
                             std::to_string(rand() % int(this->gameObject->GetApplication()->GetWindow()->getSize().y - 2 * width) + width) + " " +
-                            std::to_string(-50 * (int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 9 + 1)) +
+                            std::to_string(-50 * (int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 18 + 1)) +
                             " 0 " + std::to_string(length) + " " + std::to_string(width) + " 1 0 1");
                         this->gameObject->GetApplication()->GetStorage()->GetObject(std::string("shooting_enemy") +
                             std::to_string(int(this->gameObject->GetApplication()->GetWorkTimeAsSeconds()) / 3) + std::to_string(i + 1))->AddComponent<ReturnToField>();
